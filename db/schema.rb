e5f4a899e03381b8601f92ca22e288127cdd5102
Evan R. Murphy <evanrmurphy@gmail.com>
2010-11-13 23:18:05 -0600

    Migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101114045427) do
+ActiveRecord::Schema.define(:version => 20101114051419) do
+
+  create_table "entries", :force => true do |t|
+    t.string   "word"
+    t.text     "definition"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
