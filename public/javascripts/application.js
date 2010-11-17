5cb5f50347c71277019c369fff0f2a914c4a197c
Evan R. Murphy <evanrmurphy@gmail.com>
2010-11-17 02:34:26 -0600

    Slightly refine simple Backbone example using Word model

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,19 +1,40 @@
-word = new Backbone.Model({content : "especially"});
+Word = Backbone.Model.extend({
+
+  validate: function(attrs) {
+    if (!attrs.content.length) {
+      return "content can't be blank";
+    }
+  },
+
+  getContent: function() {
+    return this.get("content");
+  }
+                               
+});
+
+word = new Word({
+  content : "especially"
+});
+
+word.bind("error", function(model, error) {
+  alert(error);
+});
 
 WordView = Backbone.View.extend({
 
   initialize: function() {
-    _.bindAll(this, "render");
+    _(this).bindAll("render");
     this.model.bind("change", this.render);
   },
 
   render: function() {
-    $('#words').html(this.model.get("content"));
+    $(this.el).html(this.model.get("content"));
     return this;
   }
 
 });
 
-wordView = new WordView({model: word});
-
-wordView.render();
+new WordView({
+    model: word,
+    el: $('#words')
+}).render();
