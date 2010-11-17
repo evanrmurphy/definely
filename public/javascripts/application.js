39e21d6dcdf076b682da07accb5f7a9a88b1b625
Evan R. Murphy <evanrmurphy@gmail.com>
2010-11-17 03:38:19 -0600

    Make into a word list, so that changing the input field adds a new word, rather than just changing the one

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,30 +7,35 @@ word = new Word({
 
 WordView = Backbone.View.extend({
 
-  events: {
-    "change #change-word": "changeWord"
-  },
-
-  changeWord: function() {
-    this.model.set({content: $('#change-word').val()})
-    $('#change-word').val('');
-  },
-
   initialize: function() {
     _(this).bindAll("render");
     this.model.bind("change", this.render);
   },
 
   render: function() {
-    $('#word', this.el).html(this.model.get("content"));
+    $(this.el).append('<li>' + this.model.get("content"));
     return this;
   }
 
 });
 
-wordView = new WordView({
-  model: word,
-  el: $('body')
-})
+AppView = Backbone.View.extend({
 
-wordView.render();
+  events: {
+    "change #new-word": "addWord"
+  },
+
+  addWord: function() {
+    new WordView({
+      model: new Word({content: $('#new-word').val()}),
+      el: $('#words')
+    }).render();
+
+    $('#new-word').val('');
+  }
+
+});
+
+appView = new AppView({
+  el: $('body')
+});
