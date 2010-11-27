39ebaac5012fa29051b3eaab7c4700718a5cfda8
Evan R. Murphy <evanrmurphy@gmail.com>
2010-11-27 03:22:40 -0600

    Start writing application.coffee

---
+++ app/coffeescripts/application.coffee
@@ -0,0 +1,94 @@
+# Models
+
+Word = Backbone.Model
+
+# Collections
+
+WordCollection = Backbone.Collection.extend
+  model: Word
+  comparator: (word) ->
+    word.get "content"
+
+# Views
+
+WordView = Backbone.View.extend
+
+  tagName: "li"
+
+  className: "word"
+
+  events:
+    "mouseover": "toggleDestroy"
+    "mouseout": "toggleDestroy"
+    "dblclick": "edit"
+    "click .show .destroy": "destroy"
+    "change .edit input": "update"
+    "blur   .edit input": "update"
+
+  toggleDestroy: ->
+    this.$('.show .destroy').toggle()
+
+  edit: ->
+    this.$('.edit input').val( this.model.get("content") )
+    this.$('.show, .edit').toggle()
+    this.$('.edit input').focus()  # alternatively, use .select() to highlight the text
+
+  update: ->
+    this.model.set
+      content: this.$('.edit input').val()
+    this.model.change() # in case the update doesn't change the value
+
+  destroy: ->
+    this.collection.remove(this.model)
+    delete this.model
+    this.remove()
+
+  initialize: ->
+    _(this).bindAll("render")
+    this.model.bind("change", this.render)
+
+  render: ->
+    $(this.el).html(
+      _.template $('#word-template').html() )
+    this.$('.show .content').html this.model.get("content")
+    this
+
+WordsView = Backbone.View.extend
+
+  events: "change .new input": "addWord"
+
+  addWord: ->
+    this.collection.add(new Word({
+      content: this.$('.new input').val()
+    }))
+
+    this.$('.new input').val('')
+
+  render: ->
+    $(this.el).html(
+      _.template $('#words-template').html() )
+    return this
+
+# words = new WordCollection
+#
+# wordsView = new WordsView({
+#   collection: words
+# })
+#
+# wordsView.collection.bind("add", function(word) {
+#
+#   wordsView.$('.words').empty()
+#
+#   wordsView.collection.each(function(word) {
+#     wordsView.$('.words').append(
+#       new WordView({
+#         model: word,
+#         collection: words
+#       }).render().el
+#     )
+#   })
+# })
+#
+# $('body').append(
+#   wordsView.render().el
+# )
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,114 +1,65 @@
-/* Models */
-
-Word = Backbone.Model;
-
-/* Collections */
-
-WordCollection = Backbone.Collection.extend({
-  model: Word,
-
-  comparator: function(word) {
-    return word.get("content");
-  }
-});
-
-/* Views */
-
-WordView = Backbone.View.extend({
-
-  tagName: "li",
-
-  className: "word",
-
-  events: {
-    "mouseover": "toggleDestroy",
-    "mouseout": "toggleDestroy",
-    "dblclick": "edit",
-    "click .show .destroy": "destroy",
-    "change .edit input": "update",
-    "blur   .edit input": "update",
-  },
-
-  toggleDestroy: function() {
-    this.$('.show .destroy').toggle();
-  },
-
-  edit: function() {
-    this.$('.edit input').val( this.model.get("content") );
-    this.$('.show, .edit').toggle();
-    this.$('.edit input').focus();  // alternatively, use .select() to highlight the text
-  },
-
-  update: function() {
-    this.model.set({
-        content: this.$('.edit input').val()
-    });
-    this.model.change(); // in case the update doesn't change the value
-  },
-
-  destroy: function() {
-    this.collection.remove(this.model);
-    delete this.model;
-    this.remove();
-  },
-
-  initialize: function() {
-    _(this).bindAll("render");
-    this.model.bind("change", this.render);
-  },
-
-  render: function() {
-    $(this.el).html(
-      _.template( $('#word-template').html() )
-    );
-    this.$('.show .content').html( this.model.get("content") );
-    return this;
-  }
-});
-
-WordsView = Backbone.View.extend({
-
-  events: {
-    "change .new input": "addWord"
-  },
-
-  addWord: function() {
-
-    this.collection.add(new Word({
-      content: this.$('.new input').val()
-    }));
-
-    this.$('.new input').val('');
-  },
-
-  render: function() {
-    $(this.el).html(
-      _.template( $('#words-template').html() )
-    );
-    return this;
-  }
-});
-
-words = new WordCollection;
-
-wordsView = new WordsView({
-  collection: words
-});
-
-wordsView.collection.bind("add", function(word) {
-
-  wordsView.$('.words').empty();
-
-  wordsView.collection.each(function(word) {
-    wordsView.$('.words').append(
-      new WordView({
-        model: word,
-        collection: words
-      }).render().el
-    );
+(function() {
+  var Word, WordCollection, WordView, WordsView;
+  Word = Backbone.Model;
+  WordCollection = Backbone.Collection.extend({
+    model: Word,
+    comparator: function(word) {
+      return word.get("content");
+    }
   });
-});
-
-$('body').append(
-  wordsView.render().el
-);
+  WordView = Backbone.View.extend({
+    tagName: "li",
+    className: "word",
+    events: {
+      "mouseover": "toggleDestroy",
+      "mouseout": "toggleDestroy",
+      "dblclick": "edit",
+      "click .show .destroy": "destroy",
+      "change .edit input": "update",
+      "blur   .edit input": "update"
+    },
+    toggleDestroy: function() {
+      return this.$('.show .destroy').toggle();
+    },
+    edit: function() {
+      this.$('.edit input').val(this.model.get("content"));
+      this.$('.show, .edit').toggle();
+      return this.$('.edit input').focus();
+    },
+    update: function() {
+      this.model.set({
+        content: this.$('.edit input').val()
+      });
+      return this.model.change();
+    },
+    destroy: function() {
+      this.collection.remove(this.model);
+      delete this.model;
+      return this.remove();
+    },
+    initialize: function() {
+      _(this).bindAll("render");
+      return this.model.bind("change", this.render);
+    },
+    render: function() {
+      $(this.el).html(_.template($('#word-template').html()));
+      this.$('.show .content').html(this.model.get("content"));
+      return this;
+    }
+  });
+  WordsView = Backbone.View.extend({
+    events: {
+      "change .new input": "addWord",
+      addWord: function() {
+        this.collection.add(new Word({
+          content: this.$('.new input').val()
+        }));
+        return this.$('.new input').val('');
+      },
+      render: function() {
+        $(this.el).html(_.template($('#words-template').html()));
+        return this;
+      }
+    }
+  });
+}).call(this);
